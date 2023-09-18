22cd6fea2f66e701596f6a9b46a6f14083057b29
RiP3r0 <seba_k_10@onet.eu>
2023-09-18 22:48:35 +0200

    2

--- a.js
+++ a.js
@@ -1,18 +1,34 @@
-console.log("hamburger")
+/*console.log("hamburger")
 
 
 
-let x;
-x=123;
+ let x;
+ x=123;
 
-let students=20;
-//students = students + 1; students += 2;
-//students = students - 1; students -= 2;
-//students = students * 2; students *= 2;
-//students = students / 2; students /= 2;
-//let extrastudents=students % 3; 
-console.log(students)
-let g = 1+2/(3*4)
-document.write(g)
+ let students=20;
+ //students = students + 1; students += 2;
+ //students = students - 1; students -= 2;
+ //students = students * 2; students *= 2;
+ //students = students / 2; students /= 2;
+ //let extrastudents=students % 3;
 
-document.getElementById("p1").innerHTML=let;
+ console.log(students)
+    let g = 1+2/(3*4)
+    document.write(g)
+
+    let name=window.prompt("write your name")
+    document.write("Hello!\n",name," Nice to meet you!")
+
+    let username;
+    document.getElementById("button").onclick=function()
+    {
+    username=document.getElementById("text").value;
+    console.log("Hello "+username);
+    document.getElementById("label").innerHTML = username;
+    }
+*/
+
+ let age = window.prompt("How old are you?");
+ age=Number(age);
+ age += 1;
+ document.write("You are ",age," years old!");
