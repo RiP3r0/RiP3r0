5e114865773d98cbd4b84652b7177846d44cbdd6
RiP3r0 <seba_k_10@onet.eu>
2023-09-18 20:25:29 +0200

    1

---
+++ a.js
@@ -0,0 +1,18 @@
+console.log("hamburger")
+
+
+
+let x;
+x=123;
+
+let students=20;
+//students = students + 1; students += 2;
+//students = students - 1; students -= 2;
+//students = students * 2; students *= 2;
+//students = students / 2; students /= 2;
+//let extrastudents=students % 3; 
+console.log(students)
+let g = 1+2/(3*4)
+document.write(g)
+
+document.getElementById("p1").innerHTML=let;
